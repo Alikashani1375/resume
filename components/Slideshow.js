6072d4aa733288dc80348e955d3535bd076d591e
alikashani1375 <s.a.s.kashani33@gmail.com>
2023-02-20 18:41:12 +0330

    first commit

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -1,15 +1,22 @@
 
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { items } from "../public/Items.json";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import styles from "../styles/slideshow.module.css";
+import  "../styles/Slideshow.module.css";
+import $ from 'jquery';
 export default function Slideshow() {
   const { bootstrap } = items;
   const [index, setIndex] = useState(0);
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
+  useEffect(() => {
+    $(".carousel-control-prev-icon").css('background-color','black').css('border-radius',"50%")
+    $(".carousel-control-next-icon").css('background-color','black').css('border-radius',"50%")
+
+  }, [])
+  
   return (
     
   <div style={{display:'flex',flexDirection:"row",justifyContent:'center',width:'400px',margin:'auto'}}>
@@ -17,9 +24,9 @@ export default function Slideshow() {
 
     <Carousel  activeIndex={index} onSelect={handleSelect}>
       {bootstrap.map((item) => (
-        <Carousel.Item key={item.id} className={styles.itemP} interval={4000}>
+        <Carousel.Item key={item.id}  interval={4000}>
           <img src={`${item.imageUrl}`} alt="slides" style={{marginTop:'40px',width:'400px',height:'600px'}}/>
-          <Carousel.Caption className={styles.caption}>
+          <Carousel.Caption >
             <h3>{item.title}</h3>
             <p>{item.body}</p>
           </Carousel.Caption>
